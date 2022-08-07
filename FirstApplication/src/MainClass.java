d2c7197e51932105005f90aba2e35eb940e13567
deonteross24 <44271920+deonteross24@users.noreply.github.com>
2022-08-07 15:37:31 -0400

    Add files via upload

---
+++ FirstApplication/src/MainClass.java
@@ -0,0 +1,18 @@
+
+import java.util.Scanner;
+
+
+public class MainClass {
+    public static void main(String[] args){
+       /** System.out.println("Hello Deonte");
+        
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Please enter your name:  ");
+        String name = scan.nextLine();
+        System.out.println("Hello " + name);
+        scan.close();
+        **/
+        MainFrame myFrame = new MainFrame();
+        myFrame.init();
+    }
+}
